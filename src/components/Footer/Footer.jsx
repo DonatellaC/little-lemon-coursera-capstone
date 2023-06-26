6e3dde4f47ca9976beed8a811eb73ff8e2acd625
DonatellaC <donatella.carta@mailbox.org>
2023-06-26 17:11:42 +0200

    feat: update footer component and style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
 import "./Footer.css";
+import logo from "../../assets/images/logo-footer.png";
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className="footer-container">
+      <div className="footer-logo">
+        <img src={logo} alt="Little Lemon Logo Small" />
+      </div>
+      <div className="copywriting-info">
+        <p>2023 Little Lemon Restaurant. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
