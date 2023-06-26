c3f274bd91f2c53d35b9c240e37b6eeee156c4ca
DonatellaC <donatella.carta@mailbox.org>
2023-06-26 17:23:23 +0200

    feat: update nav component and style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,32 @@
 import "./Nav.css";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/*" role="button">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/*" role="button">
+            Menu
+          </a>
+        </li>
+        <li>
+          <a href="/*" role="button">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="/*" role="button">
+            Info
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
