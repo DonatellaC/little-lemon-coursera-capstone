5ed22c20994e766ea07a2898120927f185edb740
DonatellaC <donatella.carta@mailbox.org>
2023-07-07 17:14:04 +0200

    feat: add BookingForm component

--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,5 +1,12 @@
+import BookingForm from "../../components/BookingForm/BookingForm";
+
 const BookingPage = () => {
-  return <div>Booking Page </div>;
+  return (
+    <>
+      <h1> Booking Page</h1>
+      <BookingForm />
+    </>
+  );
 };
 
 export default BookingPage;
